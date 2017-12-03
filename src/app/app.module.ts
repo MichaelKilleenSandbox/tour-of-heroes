7c2c3844b491395002c10eacbf466ea80a865f90
Michael Killeen <Michael.J.Killeen@outlook.com>
2017-12-03 10:33:16 -0500

    Completed the Hero Editor tutorial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+    AppComponent,
+    HeroesComponent
+],
+imports: [
+  BrowserModule,
+  FormsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
