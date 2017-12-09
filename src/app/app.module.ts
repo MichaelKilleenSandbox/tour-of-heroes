41eda474184dfda7df52ae724a8cd993578c449e
Michael Killeen <michael.j.killeen@outlook.com>
2017-12-09 16:52:13 -0500

    Completed Routing Lesson.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { NgModule } from '@angular/core';
+import { AppRoutingModule} from './app-routing/app-routing.module';
 
 
 import { AppComponent } from './app.component';
@@ -9,22 +10,25 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService} from './service/hero.service';
 import { MessageService} from './service/message.service';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingComponent } from './app-routing/app-routing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    HeroesComponent
-,
-    HeroDetailComponent
-,
-    MessagesComponent
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
 ],
-imports: [
-  BrowserModule,
-  FormsModule
-],
-  providers: [HeroService, MessageService],
-  bootstrap: [AppComponent]
+  providers: [ HeroService, MessageService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
